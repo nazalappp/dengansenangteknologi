96613567808a999088f2cb287365ec432a05355e
nazalappp <nazala_270@smkassalaambandung.sch.id>
2022-08-12 20:07:59 +0700

    mobile pokemon

---
+++ lib/screen/app_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/main.dart';
+
+import 'package:flutter/material.dart';
+import 'package:pokemon/main.dart';
+
+class AppScreen extends StatefulWidget {
+  const AppScreen({Key? key}) : super(key: key);
+
+  @override
+  _AppScreenState createState() => _AppScreenState();
+}
+
+class _AppScreenState extends State<AppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.white);
+  }
+}
